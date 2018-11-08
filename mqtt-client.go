a082da3274d17241ce912178bccfe2f11b850ea6
RobHumphris <robhumphris@btconnect.com>
2018-11-08 10:48:39 +0000

    first cut

---
+++ mqtt-client.go
@@ -0,0 +1,138 @@
+package mqttclient
+
+import (
+	"crypto/tls"
+	"crypto/x509"
+	"fmt"
+	"io/ioutil"
+	"time"
+
+	"github.com/pkg/errors"
+
+	"github.com/RobHumphris/rf-gateway/jwt"
+
+	MQTT "github.com/eclipse/paho.mqtt.golang"
+)
+
+// MQTTClientConfig holds the essential elements needed to configure the MQTTClient
+type MQTTClientConfig struct {
+	Host              string
+	Port              string
+	RootCertFile      string
+	PrivateKeyPEMFile string
+	ProjectID         string
+	CloudRegion       string
+	RegistryID        string
+	DeviceID          string
+}
+
+// MQTTTopics hold the two Topics that the GCF cloud uses
+type MQTTTopics struct {
+	Config    string
+	Telemetry string
+}
+
+// MQTTClient contains the essential elements that the MQTT client needs to function
+type MQTTClient struct {
+	Client MQTT.Client
+	Values *MQTTClientConfig
+	Topics *MQTTTopics
+}
+
+// NewMQTTClient intialises and returns a new instance of a MQTTClient using
+// the passed MQTTClientValues and the default MessageHandler
+func NewMQTTClient(cfg *MQTTClientConfig, defaultHandler MQTT.MessageHandler) (mc *MQTTClient, err error) {
+	mc = &MQTTClient{}
+	certpool := x509.NewCertPool()
+	pemCerts, err := ioutil.ReadFile(cfg.RootCertFile)
+	if err != nil {
+		return mc, errors.Wrap(err, "Failed to read Root Cert File")
+	}
+	certpool.AppendCertsFromPEM(pemCerts)
+
+	config := &tls.Config{
+		RootCAs:            certpool,
+		ClientAuth:         tls.NoClientCert,
+		ClientCAs:          nil,
+		InsecureSkipVerify: true,
+		Certificates:       []tls.Certificate{},
+		MinVersion:         tls.VersionTLS12,
+	}
+
+	token, err := jwt.CreateJWT(cfg.ProjectID, cfg.PrivateKeyPEMFile)
+	if err != nil {
+		return mc, errors.Wrap(err, "Failed to create JWT")
+	}
+
+	opts := MQTT.NewClientOptions()
+	opts.AddBroker(getMQTTBrokerAddress(cfg))
+	opts.SetClientID(getMQTTClientID(cfg))
+	opts.SetTLSConfig(config)
+	opts.SetUsername("unused")
+	opts.SetPassword(token)
+	opts.SetDefaultPublishHandler(defaultHandler)
+
+	mc.Client = MQTT.NewClient(opts)
+	mc.Values = cfg
+	mc.Topics = getMQTTTopics(cfg)
+	return mc, nil
+}
+
+// Connect attempts to connect to the MQTT Broker and returns an error if
+// unsuccessful
+func (mc *MQTTClient) Connect() error {
+	token := mc.Client.Connect()
+	okflag := token.WaitTimeout(6 * time.Second)
+	if !okflag {
+		return fmt.Errorf("Timeout waiting to connect to")
+	}
+	if token.Error() != nil {
+		return errors.Wrap(token.Error(), "Error connecting to MQTT Broker")
+	}
+	return nil
+}
+
+// RegisterConfigHander subscribes to the Config topic, and assigns the
+// passed handler function to it.
+func (mc *MQTTClient) RegisterConfigHander(handler MQTT.MessageHandler) {
+	mc.Client.Subscribe(mc.Topics.Config, 0, handler)
+}
+
+// RegisterTelemetryHandler subscribes to the Telemetry topic, and assigns the
+// passed handler function to it.
+func (mc *MQTTClient) RegisterTelemetryHandler(handler MQTT.MessageHandler) {
+	mc.Client.Subscribe(mc.Topics.Telemetry, 0, handler)
+}
+
+// PublishTelemetryEvent sends the payload to the MQTT broker, if it doesnt
+// work we get an error.
+func (mc *MQTTClient) PublishTelemetryEvent(payload []byte) error {
+	token := mc.Client.Publish(mc.Topics.Telemetry, 0, false, payload)
+	okflag := token.WaitTimeout(5 * time.Second)
+	if !okflag {
+		return fmt.Errorf("Timeout publishing telemetry event")
+	}
+	if token.Error() != nil {
+		return errors.Wrap(token.Error(), "Error publishing telemetry event")
+	}
+	return nil
+}
+
+func getMQTTClientID(cfg *MQTTClientConfig) string {
+	return fmt.Sprintf("projects/%s/locations/%s/registries/%s/devices/%s",
+		cfg.ProjectID,
+		cfg.CloudRegion,
+		cfg.RegistryID,
+		cfg.DeviceID)
+}
+
+func getMQTTTopics(cfg *MQTTClientConfig) (topics *MQTTTopics) {
+	return &MQTTTopics{
+		Config:    fmt.Sprintf("/devices/%s/config", cfg.DeviceID),
+		Telemetry: fmt.Sprintf("/devices/%s/events", cfg.DeviceID),
+	}
+}
+
+func getMQTTBrokerAddress(cfg *MQTTClientConfig) string {
+	return fmt.Sprintf("ssl://%s:%s", cfg.Host, cfg.Port)
+}
---
+++ mqtt-client_test.go
@@ -0,0 +1,109 @@
+package mqttclient
+
+import (
+	"fmt"
+	"log"
+	"testing"
+	"time"
+
+	"github.com/RobHumphris/veh-structs/vehdata"
+	"github.com/golang/protobuf/proto"
+
+	MQTT "github.com/eclipse/paho.mqtt.golang"
+)
+
+const (
+	Host              = "mqtt.googleapis.com"
+	Port              = "8883"
+	RootCertFile      = "/home/rob/.certs/roots.pem"
+	PrivateKeyPEMFile = "/home/rob/.certs/iot_rsa_private.pem"
+	ProjectID         = "vibration-energy-harvesting"
+	CloudRegion       = "europe-west1"
+	RegistryID        = "vibration-energy-harvesting-registry"
+	DeviceID          = "dell-development-laptop"
+)
+
+func f() *vehdata.VehEvent {
+	ev := &vehdata.VehEvent{}
+	ev.Header = &vehdata.VehEventHeader{
+		Timestamp:      uint32(time.Now().Unix()),
+		SequenceNumber: 0,
+		Event:          4,
+		Length:         18,
+	}
+
+	ev.SensorEvent = &vehdata.VehSensorEvent{
+		Temperature:    23.25,
+		BatteryLevel:   1234,
+		CoulombCounter: 1,
+		Current:        2,
+	}
+	return ev
+}
+
+func testHander(client MQTT.Client, msg MQTT.Message) {
+	fmt.Printf("[handler] Topic: %v\n", msg.Topic())
+	fmt.Printf("[handler] Payload: %v\n", msg.Payload())
+}
+
+func TestClient(t *testing.T) {
+	cfg := &MQTTClientConfig{
+		Host:              Host,
+		Port:              Port,
+		RootCertFile:      RootCertFile,
+		PrivateKeyPEMFile: PrivateKeyPEMFile,
+		ProjectID:         ProjectID,
+		CloudRegion:       CloudRegion,
+		RegistryID:        RegistryID,
+		DeviceID:          DeviceID,
+	}
+
+	mc, err := NewMQTTClient(cfg, testHander)
+	if err != nil {
+		t.Errorf("Error raised in NewMQTTClient: %v\n", err)
+		return
+	}
+
+	err = mc.Connect()
+	if err != nil {
+		t.Errorf("Error raised in connecting: %v\n", err)
+		return
+	}
+
+	mc.RegisterConfigHander(func(client MQTT.Client, msg MQTT.Message) {
+		fmt.Printf("[config handler] Topic: %v\n", msg.Topic())
+		fmt.Printf("[config handler] Payload: %v\n", msg.Payload())
+	})
+
+	mc.RegisterTelemetryHandler(func(client MQTT.Client, msg MQTT.Message) {
+		fmt.Printf("[Telemetry handler] Topic: %v\n", msg.Topic())
+		fmt.Printf("[Telemetry handler] Payload: %v\n", msg.Payload())
+	})
+
+	obj := f()
+
+	for i := 0; i < 10; i++ {
+		log.Printf("[main] Publishing Message #%d", i)
+
+		obj.Header.SequenceNumber = uint32(i)
+		payload, err := proto.Marshal(obj)
+		if err != nil {
+			t.Errorf("Error marshalling Protobuffer %v", err)
+			return
+		}
+
+		err = mc.PublishTelemetryEvent(payload)
+		if err != nil {
+			t.Errorf("Error Publishing payload %v", err)
+			return
+		}
+
+		time.Sleep(1 * time.Second)
+	}
+
+	fmt.Println("Now waiting for 30 seconds, just to see what happens")
+	select {
+	case <-time.After(30 * time.Second):
+		fmt.Println("Finito")
+	}
+}
